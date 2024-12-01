cab513bc97245e4f7ef9128c33bcdfc53a87b191
Pepitoelgringo <kxzdanidp@gmail.com>
2024-12-01 08:51:17 +0100

    Cambiado a dropdownbutton para eliminar

--- lib/main.dart
+++ lib/main.dart
@@ -66,8 +66,8 @@ class HomePage extends StatefulWidget {
 /*-----------------------------------------------------------*/
 // CLASE PARA ESTABLECER LA ESTRUCTURA DE LA APLICACIÓN
 class _HomePageState extends State<HomePage> {
-  // LISTA PARA ALMACENAR TAREAS
   List<Map<String, dynamic>> _tasks = [];
+  final List<String> _dropDownButtonList = ["Completado", "No completado"];
 
   final _taskTitleController = TextEditingController();
   final _taskDescriptionController = TextEditingController();
@@ -75,60 +75,50 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    // CARGAR LAS TAREAS AL INICIAR LA PANTALLA
     _loadTasks();
   }
 
-  //FUNCIONES PARA MANEJAR LOS DATOS
+  // Función para cargar las tareas desde la base de datos
   Future<void> _loadTasks() async {
-    //COGEMOS DE LA BASE DE DATOS LAS TAREAS, 'tasks' SE REFIERE A LA TABLA DE LA BASE DE DATOS
-    final tasks = await widget.database
-        .query('tasks'); //EQUIVALE A UN SELECT * FROM tasks
+    final tasks = await widget.database.query('tasks');
     setState(() {
-      //IGUALAMOS LA LISTA DE TAREAS QUE INICIALIZAMOS VACÍA A LAS TAREAS DE LA BASE DE DATOS.
-      _tasks = tasks;
+      _tasks = tasks; // Actualizamos la lista de tareas en el estado
     });
   }
 
+  // Función para añadir una tarea
   Future<void> _addTask(String title, String description) async {
-    //INSERTAMOS EN LA TABLA tasks, EN LAS COLUMNAS title,description,completed
-    await widget.database.insert(
-        'tasks', // TABLA EN LA QUE INSERTAMOS
-        {
-          'title': title, // 'nombreColumna': ValorQueAñade
-          'description': description, // 'nombreColumna': ValorQueAñade
-          'completed': 0 // 'nombreColumna': ValorQueAñade
-        });
-    // LIMPIAMOS EL TEXTO ESCRITO EN LOS TEXTFIELDS
+    await widget.database.insert('tasks', {
+      'title': title,
+      'description': description,
+      'completed': 0, // Nueva tarea no está completada
+    });
     _taskTitleController.clear();
     _taskDescriptionController.clear();
-    // ACTUALIZAMOS LAS TAREAS
-    _loadTasks();
+    _loadTasks(); // Recargamos las tareas después de agregar una
   }
 
+  // Función para cambiar el estado de la tarea (completada o no completada)
   Future<void> _changeTaskStatus(int id, int completed) async {
     await widget.database.update(
-      'tasks', // TABLA EN LA QUE INSERTAMOS
-      {'completed': completed == 0 ? 1 : 0},
-      // SI 'completed' ES 0(NO COMPLETADO) LO CAMBIA A 1(COMPLETADO) Y VICEVERSA
-      where: 'id = ?', // ID PARA ESPECIFICAR LA TAREA QUE QUEREMOS CAMBIAR
-      whereArgs: [id], // ESTABLECEMOS EL VALOR DEL ? CON EL ID PROPORCIONADO EN LOS PARAMETROS
+      'tasks',
+      {'completed': completed}, // Actualizamos el campo 'completed'
+      where: 'id = ?',
+      whereArgs: [id],
     );
-    // ACTUALIZAMOS LAS TAREAS
-    _loadTasks();
+    _loadTasks(); // Recargamos las tareas después de cambiar el estado
   }
 
+  // Función para eliminar una tarea
   Future<void> _deleteTask(int id) async {
     await widget.database.delete(
-        'tasks', // TABLA EN LA QUE BORRAMOS
-        where: 'id = ?', // ID PARA ESPECIFICAR LA TAREA QUE QUEREMOS BORRAR
-        whereArgs: [id]); // ESTABLECEMOS EL VALOR DEL ? CON EL ID PROPORCIONADO EN LOS PARAMETROS
-    _loadTasks();
+      'tasks',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    _loadTasks(); // Recargamos las tareas después de eliminar una
   }
 
-
-
-  //ESTRUCTURA DE LA APLICACION
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -141,68 +131,85 @@ class _HomePageState extends State<HomePage> {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
+              // Campos para ingresar la tarea
               TextField(
                 controller: _taskTitleController,
                 decoration: InputDecoration(hintText: 'Nombre de la tarea'),
               ),
               TextField(
                 controller: _taskDescriptionController,
-                decoration:
-                    InputDecoration(hintText: 'Descripción de la tarea'),
+                decoration: InputDecoration(hintText: 'Descripción de la tarea'),
               ),
               SizedBox(height: 20),
-              Center(
-                child: ElevatedButton(
-                    onPressed: () {
-                      _addTask(_taskTitleController.text, _taskDescriptionController.text);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      iconColor: Colors.white,
-                    ),
-                    child: const Row(
-                      mainAxisSize: MainAxisSize.min,
-                      //CAMBIA EL TAMAÑO DEL BOTON AL MINIMO POSIBLE
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.add),
-                        Text(
-                          "Añadir",
-                          style: TextStyle(color: Colors.white),
-                        )
-                      ],
-                    )),
+              ElevatedButton(
+                onPressed: () {
+                  _addTask(
+                    _taskTitleController.text,
+                    _taskDescriptionController.text,
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  iconColor: Colors.white,
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.add),
+                    Text("Añadir", style: TextStyle(color: Colors.white)),
+                  ],
+                ),
               ),
+              // Lista de tareas
               Expanded(
                 child: ListView.builder(
-                    itemCount: _tasks.length,
-                    padding: EdgeInsets.all(10),
-                    itemBuilder: (context, int index) {
-                      final task = _tasks[index];
-                      return ListTile(
-                        leading: IconButton(
-                          icon: Icon(task['completed'] == 1 ? Icons.check_box : Icons.check_box_outline_blank),
-                          onPressed: () {
-                            _changeTaskStatus(task['id'], task['completed']);
-                          },
-                        ),
-                        title: Text("${task['title']} \n${task['description']}",
-                          style: TextStyle(
-                            decoration: task['completed'] == 1
-                                ? TextDecoration.lineThrough
-                                : TextDecoration.none,
-                            color: task['completed'] == 1
+                  itemCount: _tasks.length,
+                  padding: EdgeInsets.all(10),
+                  itemBuilder: (context, int index) {
+                    final task = _tasks[index];
+                    return ListTile(
+                      leading: Text(
+                        "${task['title']} \n${task['description']}",
+                        style: TextStyle(
+                          decoration: task['completed'] == 1
+                              ? TextDecoration.lineThrough
+                              : TextDecoration.none,
+                          color: task['completed'] == 1
                               ? Colors.grey
-                                : Colors.black
-                          ),
+                              : Colors.black,
                         ),
-                        trailing: IconButton(
+                      ),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          // DropdownButton para cambiar el estado de la tarea
+                          DropdownButton<String>(
+                            value: task['completed'] == 1 ? "Completado" : "No completado", // Selección basada en el estado de 'completed'
+                            items: _dropDownButtonList.map((String item) {
+                              return DropdownMenuItem<String>(
+                                value: item,
+                                child: Text(item),
+                              );
+                            }).toList(),
+                            onChanged: (String? newValue) {
+                              // Convertimos el valor del Dropdown en un int para actualizar la base de datos
+                              int completedValue = newValue == "Completado" ? 1 : 0;
+                              _changeTaskStatus(task['id'], completedValue);
+                            },
+                          ),
+                          IconButton(
                             onPressed: () {
                               _deleteTask(task['id']);
-                            }, icon: const Icon(Icons.delete)),
-                      );
-                    }),
-              )
+                            },
+                            icon: Icon(Icons.delete),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
@@ -210,3 +217,4 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
